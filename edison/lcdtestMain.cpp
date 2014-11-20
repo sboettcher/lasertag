d15e995e5a6a40b7fbbb20580922622a8b510d83
Sebastian Boettcher <seb.boett@gmail.com>
2014-11-20 16:00:40 +0000

    small changes, display works now

--- edison/lcdtestMain.cpp
+++ edison/lcdtestMain.cpp
@@ -9,7 +9,10 @@
 #include <mraa.hpp>
 #include <ssd1327.h>
 
-#define BUS_NUMBER 0
+#include <iostream>
+#include <string>
+
+#define BUS_NUMBER 6
 
 int running = 1;
 
@@ -21,17 +24,27 @@ void sig_handler(int signo) {
 }
 
 int main(int argc, char** argv) {
-  signal(SIGINT, sig_handler);
+  // signal(SIGINT, sig_handler);
 
+  printf("instantiating/initialising... ");
+  fflush(stdout);
   upm::SSD1327* lcd = new upm::SSD1327(BUS_NUMBER);
+  printf("done.\n");
+  fflush(stdout);
 
   for (uint8_t i = 0; i < 12; ++i) {
     lcd->setCursor(i, 0);
     lcd->setGrayLevel(i);
     lcd->write("Hello World");
+    printf("Hello World\n");
+    fflush(stdout);
   }
 
+  printf("closing... ");
+  fflush(stdout);
   lcd->close();
+  printf("done.\n");
+  fflush(stdout);
 
   return 0;
 }
