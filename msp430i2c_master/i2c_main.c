4f460a0bfac8c34688735912348097485e17b44f
Jonas <jonas.schlagenhauf@gmx.de>
2014-12-10 13:26:03 +0100

    More changes to master. UART is now part of i2c_master.c .

--- msp430i2c_master/i2c_main.c
+++ msp430i2c_master/i2c_main.c
@@ -11,6 +11,14 @@ void initPins()
   BCSCTL1 = CALBC1_16MHZ;
   DCOCTL  = CALDCO_16MHZ;
 
+  P1SEL |= BIT1 + BIT2;                      // P1.1 = RXD, P1.2=TXD
+  P1SEL2 |= BIT1 + BIT2;                     // P1.1 = RXD, P1.2=TXD
+  UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+  UCA0BR0 = (1667 & 0xFF);                   // 8MHz 9600
+  UCA0BR1 = (1667 >> 8);                     // 8MHz 9600
+  UCA0MCTL = UCBRS0;                        // Modulation UCBRSx = 1
+  UCA0CTL1 &= ~UCSWRST;                     // Initialize USCI state machine
+  IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
 }
 
 /*
@@ -21,7 +29,10 @@ int main(void)
   initPins();
 
   unsigned char data[] = {0x3B};
-  unsigned char indata[10];
+  unsigned char indata[5];
+  indata[4] = '\0';
+
+  serialPrintln("MSP I2C Master booted and sending.");
 
   while(1)
   {
@@ -32,10 +43,14 @@ int main(void)
     //while(!TI_USCI_I2C_ready());
     */
 
-    master_i2c_receive_init(0x68, 32);
+    master_i2c_receive_init(0x68, 1600);
+    //master_i2c_receive_init(0x68, 32);
     while(!i2c_ready());
     master_i2c_receive(4, indata);
     while(!i2c_ready());
+
+    serialPrint("Got i2c data: ");
+    serialPrintln(indata);
 
 
   }
--- msp430i2c_master/i2c_master.c
+++ msp430i2c_master/i2c_master.c
@@ -10,10 +10,23 @@
 #define SDA_PIN BIT7                                  // msp430g2553 UCB0SDA pin
 #define SCL_PIN BIT6                                  // msp430g2553 UCB0SCL pin
 
-
+// i2c variables
 signed char byteCtr;
 unsigned char *TI_receive_field;
 unsigned char *TI_transmit_field;
+
+// uart variables
+#define RXBUFFERSIZE 32
+
+// Serial ringbuffer & bufferpositions
+char volatile rxBuffer[RXBUFFERSIZE];
+char volatile rxBufferStart = 0;
+char volatile rxBufferEnd = 0;
+// Error flag for serial buffer
+char rxBufferError = 0;
+// Echo flag - 1 if we should echo back the serial input
+char echoBack = 0;
+
 
 //------------------------------------------------------------------------------
 // void TI_USCI_I2C_receiveinit(unsigned char slave_address,
@@ -112,6 +125,130 @@ unsigned char i2c_ready(){
   return !(UCB0STAT & UCBBUSY);
 }
 
+// UART functions
+// 1 if serial input should be echoed again, 0 if not
+void serialEchoBack(char e)
+{
+  echoBack = (!e)?0:1;
+}
+
+// Returns 1 if there's been an overflow, 0 if not.
+char serialError()
+{
+  char r = rxBufferError;
+  rxBufferError = 0;
+  return r;
+}
+
+// Print one character (given by <tx>) on UART
+void serialWrite(char tx)
+{
+  // TX buffer ready?
+  while (!(IFG2&UCA0TXIFG));
+  // Print byte
+  UCA0TXBUF = tx;
+  // Wait for transmit
+  while (!(IFG2&UCA0TXIFG));
+}
+
+// Print integer as readable number to UART (using ASCII)
+// Ignore leading zeros.
+void serialPrintInt(int i)
+{
+  int j = i;
+  if (j > 10000)
+    serialWrite(0x30 + i/10000);
+  i = i % 10000;
+  if (j > 1000)
+    serialWrite(0x30 + i/1000);
+  i = i % 1000;
+  if (j > 100)
+    serialWrite(0x30 + i/100);
+  i = i % 100;
+  if (j > 10)
+    serialWrite(0x30 + i/10);
+  i = i % 10;
+  serialWrite(0x30 + i/1);
+}
+
+// Print chars given by <tx> on UART
+void serialPrint(char* tx)
+{
+  int b, i = 0;
+  while(tx[i] != 0x00)
+    i++;
+  for (b = 0; b < i; b++)
+    serialWrite(tx[b]);
+}
+
+// Print chars given by <tx> on UART and print \n afterwards.
+void serialPrintln(char* tx)
+{
+  // Print <tx>
+  serialPrint(tx);
+  // Print \n\r
+  serialWrite(0x0D);
+  serialWrite(0x0A);
+}
+
+// Return 1 if there's serial data available
+char serialAvailable(void)
+{
+  if (rxBufferStart != rxBufferEnd)
+    return 1;
+  return 0;
+}
+
+// Clear all received bytes.
+void serialFlush(void)
+{
+  rxBufferStart = rxBufferEnd;
+}
+
+// Returns the first byte from the ringbuffer but does not delete it. Returns -1 if no data is available.
+int serialPeek(void)
+{
+  if (rxBufferStart == rxBufferEnd)
+    return -1;
+  return rxBuffer[rxBufferStart];
+}
+
+// Returns the first byte from the ringbuffer and deletes it. Returns -1 if no data is available.
+int serialRead(void)
+{
+  if (rxBufferStart == rxBufferEnd)
+    return -1;
+  char r = rxBuffer[rxBufferStart++];
+  rxBufferStart %= RXBUFFERSIZE;
+  return r;
+}
+
+// Reads in a number from the serialInterface
+// This is a very basic implementation and you might want to write your own
+// depending on your scenario
+int serialReadInt(void)
+{
+  int number = 0;
+  char stop = 0;
+  char negative = 0;
+  while (!stop)
+  {
+    while (!serialAvailable());
+    char letter = serialRead();
+    if (letter == '-' && number == 0)
+      negative = 1;
+    else if (letter >= '0' && letter <= '9')
+      number = number * 10 + (letter - '0');
+    else
+      stop = 1;
+  }
+  if (negative)
+    return number * -1;
+  return number;
+}
+
+// interrupt routines
+
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
   #pragma vector = USCIAB0RX_VECTOR
   __interrupt void USCIAB0RX_ISR(void)
@@ -136,6 +273,7 @@ unsigned char i2c_ready(){
   #error Compiler not supported!
 #endif
 {
+  // interrupts in module B (i2c)
   if (IFG2 & UCB0RXIFG){
     if ( byteCtr == 0 ){
       UCB0CTL1 |= UCTXSTP;                    // I2C stop condition
@@ -158,5 +296,21 @@ unsigned char i2c_ready(){
       TI_transmit_field++;
       byteCtr--;
     }
+  }
+
+  // interrupts in module A (UART)
+  if (IFG2 & UCB0RXIFG)
+  {
+    // Store received byte in ringbuffer
+    rxBuffer[rxBufferEnd++] = UCA0RXBUF;
+    rxBufferEnd %= RXBUFFERSIZE;
+    if (echoBack)
+    {
+      while (!(IFG2&UCA0TXIFG));
+      UCA0TXBUF = UCA0RXBUF;
+    }
+    // Check for an overflow
+    if (rxBufferStart == rxBufferEnd)
+      rxBufferError = 1;
   }
 }
