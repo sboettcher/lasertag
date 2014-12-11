d4a4b2965a32667fec7804d0d930188edfefe925
Sebastian Boettcher <seb.boett@gmail.com>
2014-12-11 14:04:12 +0000

    playing around with some lcd stuff

--- edison/lasertagMain.cpp
+++ edison/lasertagMain.cpp
@@ -8,13 +8,19 @@
 #include <syslog.h>
 
 #include <mraa.hpp>
+#include <ssd1327.h>
 
 #include <string>
 #include <iostream>
+#include <sstream>
 
+#define I2C_BUS 6
 
 int running = 1;
+uint8_t ammo = 75;
+bool active = false;
 
+upm::SSD1327* lcd;
 
 void sig_handler(int signo) {
   if (signo == SIGINT) {
@@ -22,22 +28,67 @@ void sig_handler(int signo) {
   }
 }
 
+void lcd_write_int(int i, int x, int y) {
+  lcd->setCursor(x, y);
+  std::stringstream ss;
+  ss << i;
+  lcd->write(ss.str());
+}
+
+void write_ammo() {
+  if (ammo < 100) {
+    lcd_write_int(0, 1, 6);
+    lcd_write_int(ammo, 1, 7);
+  } else {
+    lcd_write_int(ammo, 1, 6);
+  }
+}
+
+void write_status() {
+  lcd->setCursor(0, 2);
+  if (active) {
+    lcd->write(" active ");
+  } else {
+    lcd->write("inactive");
+  }
+}
 
 int main(int argc, char** argv) {
   signal(SIGINT, sig_handler);
 
-  mraa::I2c* i2c = new mraa::I2c(6);
+  printf("init display... ");
+  fflush(stdout);
+  lcd = new upm::SSD1327(I2C_BUS);
+  printf("Done.\n");
+  fflush(stdout);
+  lcd->setGrayLevel(255);
+
+  write_status();
+  lcd->setCursor(1, 0);
+  lcd->write("ammo: ");
+  write_ammo();
+  active = true;
+  write_status();
+
+  mraa::I2c* i2c = new mraa::I2c(I2C_BUS);
   i2c->address(0x68);
+
+  uint8_t i = 2;
 
   while (running == 1) {
     int rec = i2c->read();
     if (rec != 0) {
       printf("%d\n", rec);
       fflush(stdout);
+
+      lcd_write_int(rec, i++, 0);
+      if (i > 10)
+        i = 2;
     }
     usleep(1000);
   }
 
+  lcd->close();
   printf("\nDone.\n");
   return 0;
 }
