7ddb88fddada2956ce4e31030ebf4e855bdc00a4
Sebastian Boettcher <seb.boett@gmail.com>
2014-12-23 18:56:56 +0000

    small fix

--- edison/src/groveOLEDtestMain.cpp
+++ edison/src/groveOLEDtestMain.cpp
@@ -12,7 +12,7 @@
 #include <iostream>
 #include <string>
 
-#define BUS_NUMBER 6
+#define BUS_NUMBER 1
 
 int running = 1;
 
@@ -26,7 +26,7 @@ void sig_handler(int signo) {
 int main(int argc, char** argv) {
   // signal(SIGINT, sig_handler);
 
-  printf("instantiating/initialising... ");
+  printf("initialising on bus %d... ", BUS_NUMBER);
   fflush(stdout);
   upm::SSD1327* lcd = new upm::SSD1327(BUS_NUMBER);
   printf("done.\n");
